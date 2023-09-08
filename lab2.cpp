b1aa909478806aa950650bb75453f621a186e3ac
qx1331 <113266222+qx1331@users.noreply.github.com>
2023-09-08 16:18:38 +0300

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -10,8 +10,8 @@ using namespace std;
 int main() {
     double x = 17.421, y = 10.365, z = 0.828;
 
-    double numerator = pow(y + (cbrt(x) - 1), 1.0/4.0);
-    double denominator = (abs(x - y)) * (pow(sin(z), 2.0) + tan(z));
+    double numerator = pow(y + cbrt(x - 1.), 1./4.);
+    double denominator = (abs(x-y)) * (pow(sin(z), 2.) + tan(z));
     cout << numerator/denominator;
 
     return 0;
